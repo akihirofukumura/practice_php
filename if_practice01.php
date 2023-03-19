1fd54dbaa88797911bc2915c8814d48bf84f1114
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-03-19 15:51:29 +0900

    if [age 0 ~ 10 = boy] output

---
+++ if_practice01.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>If-plactice</title>
+</head>
+<body>
+  <h1>if文を使って年齢によって表示方法を変える</h1>
+  <form action="" method="post">
+    <input type="text" name="age">
+    <input type="submit" value="submit" name="btn">
+  </form>
+</body>
+</html>
+
+<?php
+  if (isset($_POST['btn'])) {
+    $age = $_POST['age'];
+    if ($age > 0 && $age <=10) {
+      echo "boy";
+    }
+  }
+?>
